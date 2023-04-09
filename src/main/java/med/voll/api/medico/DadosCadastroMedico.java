217d96b7bed5708b6b7f0aa9bc78446278055012
Edson Minoru Yamazaki <edson.minoru@muralis.com.br>
2023-04-09 14:02:11 -0300

    Adicionado validação com as annotation do Bean Validation

--- src/main/java/med/voll/api/medico/DadosCadastroMedico.java
+++ src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -1,6 +1,28 @@
 package med.voll.api.medico;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import med.voll.api.endereco.DadosEndereco;
 
-public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade, DadosEndereco endereco) {
+public record DadosCadastroMedico(
+        @NotBlank // Verifica se está null or empty
+        String nome,
+        @NotBlank
+        @Email
+        String email,
+
+        @NotBlank
+        String telefone,
+
+        @NotBlank
+        @Pattern(regexp = "\\d{4,6}") // de 4 a 6 dígitos
+        String crm,
+        @NotNull // Not blank é só para campos String
+        Especialidade especialidade,
+        @NotNull
+        @Valid // Validar outro objeto que tbm terá anotações de validações
+        DadosEndereco endereco) {
 }
