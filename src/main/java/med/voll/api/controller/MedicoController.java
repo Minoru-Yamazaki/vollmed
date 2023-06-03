2874b46945f59d0d6478b8b8ac83a144e640e46f
Edson Minoru Yamazaki <edson.minoru@muralis.com.br>
2023-06-03 13:04:19 -0300

    feat: Adicionado os métodos detalhar/listar/atualizar/excluir.

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,15 +1,18 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
-import med.voll.api.medico.DadosCadastroMedico;
-import med.voll.api.medico.Medico;
+import med.voll.api.domain.medico.DadosListagemMedicos;
+import med.voll.api.domain.medico.Medico;
+import med.voll.api.domain.medico.*;
 import med.voll.api.repository.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/medicos")
@@ -20,8 +23,49 @@ public class MedicoController {
 
     @PostMapping
     @Transactional // método de escrita nessecita de uma transação ativa
-    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){
-        repository.save(new Medico(dados));
+    public ResponseEntity cadastrar(@RequestBody @Valid DadosCadastroMedico dados, UriComponentsBuilder uriBuilder){
+        Medico medico = new Medico(dados);
+        repository.save(medico);
+
+        var uri = uriBuilder.path("/medicos/{id}").buildAndExpand(medico.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(new DadosDetalhamentoMedido(medico));
     }
 
+    @GetMapping
+    public ResponseEntity<Page<DadosListagemMedicos>> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao){ // Caso não seja informado nenhum parâmetro de ordenação ou paginação será ordenado/paginado conforme definido na annotation PageableDefault
+        // http://localhost:8080/medicos?size=1 -> size=1 no endereço da requisição para retornar 1 registro por página
+        // Caso não passe o parâmetro size por padrão o spring retorna o máx de 20 registros
+        // http://localhost:8080/medicos?page=0 parâmetro page controla qual página carregar
+        // http://localhost:8080/medicos?sort=nome parâmetro sort controla a ordenação, nessa caso será ordenado pelo nome
+        // por padrão o Spring ordena de forma crescente, para ordenar de forma decrescente: sort=atributo,desc
+        var page =  repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedicos::new);
+
+        return ResponseEntity.ok(page);
+    }
+
+    @PutMapping
+    @Transactional
+    public ResponseEntity atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados){
+        var medico = repository.getReferenceById(dados.id());
+        medico.atualizarInformacoes(dados);
+
+        return ResponseEntity.ok(new DadosDetalhamentoMedido(medico));
+    }
+
+    @Transactional
+    @DeleteMapping("/{id}")
+    public ResponseEntity excluir(@PathVariable Long id){
+        var medico = repository.getReferenceById(id);
+        medico.inativar();
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity detalhar(@PathVariable Long id){
+        var medico = repository.getReferenceById(id);
+
+        return ResponseEntity.ok(new DadosDetalhamentoMedido(medico));
+    }
 }
