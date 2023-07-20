9be1e3c728e0513aeb1af6bc4cd512c25061a571
Edson Minoru Yamazaki <edson.minoru@muralis.com.br>
2023-07-19 21:16:14 -0300

    feat: strategies para validação dos dados de agendamento da consulta

---
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorMedicoComOutraConsultaNoMesmoHorario.java
@@ -0,0 +1,24 @@
+package med.voll.api.domain.consulta.validacoes;
+
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import med.voll.api.domain.exception.ValidacaoException;
+import med.voll.api.repository.ConsultaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ValidadorMedicoComOutraConsultaNoMesmoHorario implements ValidadorAgendamentoDeConsulta{
+
+    @Autowired
+    private ConsultaRepository repository;
+
+    @Override
+    public void validar(DadosAgendamentoConsulta dados){
+
+        var medicoPossuiOutraConsultaNoMesmoHorario = repository.existsByMedicoIdAndData(dados.idMedico(), dados.data());
+
+        if (medicoPossuiOutraConsultaNoMesmoHorario) {
+            throw new ValidacaoException("Médico já possui outra consulta agendada nesse horário");
+        }
+    }
+}
---
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorPacienteAtivo.java
@@ -0,0 +1,23 @@
+package med.voll.api.domain.consulta.validacoes;
+
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import med.voll.api.domain.exception.ValidacaoException;
+import med.voll.api.repository.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ValidadorPacienteAtivo implements ValidadorAgendamentoDeConsulta{
+
+    @Autowired
+    private PacienteRepository repository;
+
+    @Override
+    public void validar(DadosAgendamentoConsulta dados){
+
+        var medicoEstaAtivo = repository.findAtivoById(dados.idMedico());
+        if (!medicoEstaAtivo) {
+            throw new ValidacaoException("Consulta não pode ser agendada com paciente excluído.");
+        }
+    }
+}
